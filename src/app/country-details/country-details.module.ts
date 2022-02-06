ff9488b3f80334fd48446cc3f353130c41cfd8d9
cheekezie <harrychikezie1@gmail.com>
2022-02-06 02:29:40 +0100

    detail page updated

--- src/app/country-details/country-details.module.ts
+++ src/app/country-details/country-details.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CountryDetailsComponent } from './country-details.component';
+import { CountryDetailsRoutingModule } from './country-details-routing.module';
 
 @NgModule({
   declarations: [CountryDetailsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, CountryDetailsRoutingModule],
 })
 export class CountryDetailsModule {}
